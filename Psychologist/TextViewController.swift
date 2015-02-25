42ce953c65316b886858b6654acf274630980333
hugofalkman <zilla@Hs-MacBook-Pro.local>
2015-02-25 16:36:08 +0100

    Lecture 7 complete

---
+++ Psychologist/TextViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TextViewController.swift
+//  Psychologist
+//
+//  Created by H Hugo Falkman on 2015-02-25.
+//  Copyright (c) 2015 H Hugo Fakman. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            textView.text = text
+        }
+    }
+    
+    var text: String = "" {
+        didSet {
+            textView?.text = text
+        }
+    }
+    
+    override var preferredContentSize: CGSize {
+        get {
+            if textView != nil && presentingViewController != nil {
+                return textView.sizeThatFits(presentingViewController!.view.bounds.size)
+            } else {
+                return super.preferredContentSize
+            }
+        }
+        set {super.preferredContentSize = newValue}
+    }
+    
+}
